6b96302c51ba864dccc0fe0677da99e1712c5e66
XIE,JIA-HONG <0701phantom@gmail.com>
2024-09-22 17:28:43 +0800

    add image query

--- main.py
+++ main.py
@@ -1,62 +1,72 @@
 import streamlit as st
-import time
-from ollama import Client
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+import base64
 
-client = Client()
+load_dotenv()
 
+# Initialize the session state
+if "model" not in st.session_state:
+    st.session_state["model"] = "gpt-4o-mini-2024-07-18"
+if "client" not in st.session_state:
+    st.session_state["client"] = OpenAI(
+        api_key=os.getenv("OPENAI_API_KEY")
+    )
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+if "images" not in st.session_state:
+    st.session_state["images"] = None
+# get response from the model
+def invoke() -> str:
+    messages = st.session_state["messages"]
+    if st.session_state["images"] is not None:
+        query = messages[-1]["content"]
+        base64_string=base64.b64encode(st.session_state["images"].getvalue()).decode('utf-8')
+        messages.append(
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": f"{query}"},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/png;base64,{base64_string}"
+                        }
+                    }
+                ]
+            }
+        )
+    stream = st.session_state["client"].chat.completions.create(
+        model=st.session_state["model"],
+        messages=messages,
+        stream=True
+    )
+    if st.session_state["images"] is not None:
+        st.session_state["messages"].pop()
+    return st.write_stream(stream)
 
-# get response from llama3.1:8b model
-def invoke(query: str, history: list[dict]) -> str:
-    response = client.chat(model="llama3.1:8b", messages=history)
-    return response["message"]["content"]
+st.title("Chatbot")
 
-
-# streaming response
-def streaming_resopnse(resopnse: str):
-    for word in resopnse:
-        yield word
-        time.sleep(0.05)
-
-
-# Initialize the chat history
-if "history" not in st.session_state:
-    st.session_state.history = [
-        {"role": "system", "content": "回答時請使用#zh-TW"},
-    ]
-if "query" not in st.session_state:
-    st.session_state.user_input = ""
-if "processing" not in st.session_state:
-    st.session_state.processing = False
-
-st.title("llama3.1:8b chatbot🦙")
+MODEL_OPTIONS = ["gpt-4o-mini-2024-07-18"]
+with st.sidebar:
+    st.session_state["model"] = st.selectbox("Select Model", MODEL_OPTIONS)
+    st.subheader(f"Model: {st.session_state['model']} is selected")
+    st.session_state["images"]=st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+    
 # Display the chat history
-for message in st.session_state.history[1:]:
+for message in st.session_state["messages"]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-
-if not st.session_state["processing"]:
-    input = st.chat_input("傳訊息給 llama🦙")
-    if input:
-        st.session_state["query"] = input
-        st.session_state["processing"] = True
-        st.rerun()
 # Process the user input
-if st.session_state["processing"]:
-    query = st.session_state["query"]
-    # Display the chat input not editable
-    st.chat_input("傳訊息給 llama🦙", disabled=True)
-    # Display the user input
+if query:=st.chat_input():
+    st.session_state["messages"].append({"role": "user", "content":query})
     with st.chat_message("user"):
         st.markdown(query)
-        st.session_state["history"].append({"role": "user", "content": query})
     # Get the response from the chatbot
     with st.chat_message("assistant"):
-        with st.spinner("Processing..."):
-            response = invoke(query, st.session_state["history"])
-            st.markdown(st.write_stream(streaming_resopnse(response)))
-            st.session_state["history"].append(
-                {"role": "assistant", "content": response}
+        response = invoke()
+        st.session_state["messages"].append(
+            {"role": "assistant", "content":response}
             )
-    st.session_state["query"] = ""
-    st.session_state["processing"] = False
-    st.rerun()
+        print(st.session_state["messages"])
