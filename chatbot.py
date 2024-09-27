cab4541e74b596169fe317e8189cb016285c3524
XIE,JIA-HONG <64515959+JIAHONG0824@users.noreply.github.com>
2024-09-27 15:50:47 +0800

    update response prompt

--- chatbot.py
+++ chatbot.py
@@ -3,8 +3,8 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 import base64
-from document_relevant import relevant
-from retrieval import is_retrieval_needed
+from relevant_document import relevant
+from evaluator import is_retrieval_needed
 
 load_dotenv()
 
@@ -17,7 +17,7 @@ if "client" not in st.session_state:
     )
 if "images" not in st.session_state:
     st.session_state["images"] = None
-if "context" not in st.session_state:
+if "documens" not in st.session_state:
     st.session_state["context"] = []
 if "retrieval" not in st.session_state:
     st.session_state["retrieval"] = False
@@ -30,21 +30,16 @@ def invoke(query) -> str:
         }
     ]
     if st.session_state["retrieval"]:
-        print("Retrieval is needed")
-        print(st.session_state["context"])
         template=[
             {
                 "role": "system",
                 "content": """
-                請根據以下的 context 內容回答使用者的問題。
-                如果 context 中包含與問題相關的資訊，請根據這些資訊提供答案；
-                如果 context 中沒有相關內容，請回答「沒有足夠的資訊，無法回答」。
-                請嚴格遵守這個指示。
+                根據使用者的問題，參考 context 中的內容回答。如果 context 中的資訊與問題不匹配或提供的資訊不同，請明確指出不一致的地方。如果 context 中沒有資料，回答「我需要求助外部資訊，才有辦法回答您」。請嚴格遵守指示，避免引入錯誤資訊。
                 """ 
             },
             {
                 "role": "user",
-                "content": f"context: {"\n".join(st.session_state['context'])}\n\n 使用者問題: {query}"
+                "content": f"context:\n{"\n\n".join(st.session_state['context'])}\n\n 使用者問題:\n{query}"
             }
         ]
     stream = st.session_state["client"].chat.completions.create(
@@ -69,8 +64,10 @@ if query:=st.chat_input():
     with st.chat_message("assistant"):
         with st.status("Thinking...",expanded=True):
             if is_retrieval_needed(query)== "需要":
+                print("Retrieval is needed")
                 st.session_state["retrieval"]=True
                 st.session_state["context"]=relevant(query)
             else:
+                print("Retrieval is not needed")
                 st.session_state["retrieval"]=False
             response = invoke(query)
